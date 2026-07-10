f6a635902586d31aeed5cb1b21d8596fb80314b7
Pradip Nayak <pradipn962@gmail.com>
2026-07-10 22:11:20 +0530

    JBD Day - 02

---
+++ app.js
@@ -0,0 +1,43 @@
+// let todo = [];
+// let req = prompt("Please Enter Your Request:");
+
+// while (true) {
+//     if (req === "quit") {
+//         console.log("Quitting App");
+//         break;
+//     }
+
+//     if (req === "list") {
+//         console.log("---------------------------");
+
+//         for (let i = 0;i<todo.length;i++){
+//             console.log(i,todo[i]);
+//         }
+
+//         console.log("---------------------------");
+//     }
+
+//     else if (req === "add") {
+//         let task = prompt("Please Add Your Task");
+//         todo.push(task);
+//         console.log("Task Added");
+//     }
+//     else if(req === "delete"){
+//         let idx = prompt("Please Enter the task index")
+//         todo.splice(idx,1)
+//     }
+//     else{
+//         console.log("Wrong Request");
+        
+//     }
+
+//     req = prompt("Please Enter Your Request:").toLowerCase();
+// }
+
+
+let Day02 = {
+    name :"Javascript",
+    topic:"Object in js",
+    status:"200"
+};
+console.log(Day02)
